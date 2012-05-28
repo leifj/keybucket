aaac95bc5c9575aa7eb3b983e39a359a7d043722
Johan Berggren <jbn@nordu.net>
2012-05-28 17:31:03 +0200

    Check profile on login, populate with hash and idp

--- keylocker/profile/models.py
+++ keylocker/profile/models.py
@@ -1,3 +1,5 @@
+import hashlib
+from django.contrib.auth.signals import user_logged_in
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,3 +7,17 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User,related="profile")
     idp = models.CharField(max_length=256)
     uhash = models.CharField(max_length=256,unique=True) # sha1 of user.username
+
+def populate_profile(sender, user, request, **kwargs):
+    """
+    Check if profile exists when user logs in, otherwise create it.
+    Also populates idp from the request.
+    """
+    profile = UserProfile.objects.get_or_create(user=user)
+    if not profile.uhash:
+        profile.uhash = hashlib.sha1(user.username).hexdigest()
+        profile.save()
+    #ToDo get idp from request and set
+    return
+
+user_logged_in.connect(populate_profile)

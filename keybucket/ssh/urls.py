b359cb93db591331f806d323cc4aee2e7af35559
Leif Johansson <leifj@sunet.se>
2012-05-31 14:29:38 +0200

    fix urls.py bug

--- keybucket/ssh/urls.py
+++ keybucket/ssh/urls.py
@@ -5,9 +5,9 @@ from django.views.generic import ListView
 from keybucket.ssh.models import SSHKey
 
 urlpatterns = patterns('keybucket.ssh.views',
-    url(r'^/?$','list_keys'),
-    url(r'^add/?$','add_key'),
-    url(r'^remove/(?P<kid>[0-9]+)/?$'),
-    url(r'^(?P<utag>[^+]+)\+(?P<labels>.+)/?$','get_in_authorized_keys_fmt'),
-    url(r'^(?P<utag>[^+]+)/?$','get_in_authorized_keys_fmt'),
+    url(r'^/?$',"list_keys"),
+    url(r'^add/?$',"add_key"),
+    url(r'^remove/(?P<kid>[0-9]+)/?$', "remove_key"),
+    url(r'^(?P<utag>[^+]+)\+(?P<labels>.+)/?$',"get_in_authorized_keys_fmt"),
+    url(r'^(?P<utag>[^+]+)/?$',"get_in_authorized_keys_fmt"),
 )

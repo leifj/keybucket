1d1af7148b2400a67a5dcb3b5367f370b842c858
Leif Johansson <leifj@sunet.se>
2012-05-31 14:36:39 +0200

    add debug and cleaup more imports

--- keybucket/ssh/forms.py
+++ keybucket/ssh/forms.py
@@ -4,9 +4,9 @@ Created on May 28, 2012
 @author: leifj
 '''
 from django.forms.models import ModelForm
-from ssh.models import SSHKey
+from keybucket.ssh.models import SSHKey
 from django import forms
-from assurance.models import Assurance
+from keybucket.assurance.models import Assurance
 from django.forms.widgets import CheckboxInput, CheckboxSelectMultiple
 
 class SSHKeyForm(ModelForm):
